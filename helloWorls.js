3fe9e98ef57b34ad2b189c5a8521869cce0f3871
Lughnizaid <56219460+Lughnizaid@users.noreply.github.com>
2021-10-29 16:30:09 +0200

    fix: adding new login

--- helloWorls.js
+++ helloWorls.js
@@ -32,7 +32,15 @@ console.Log("Commit 1");
 
 console.Log("Commit 2");
 
+console.Log("Commit 3");console.Log("Commit 1");
+
+console.Log("Commit 2");
+
 console.Log("Commit 3");
+
+console.Log("Commit 4");
+
+console.Log("Commit 5");
 
 console.Log("Commit 4");
 
