e8ad8eae1cca30350c306b895c2f593ffe13fcfb
Lughnizaid <56219460+Lughnizaid@users.noreply.github.com>
2021-11-11 10:36:19 +0100

    feat: add new text message

--- new.vue
+++ new.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
-    <p> Test 1 </p>
-    <p> Test 2 </p>
-    <p> Test 2 </p>
-    <p> Test 2 </p>
-    <p> Test 2 </p>
-    <p> Test 2 </p>
-    <p> Test 2 </p>
-    <p> Test 2 </p>
-    <p> Test 2 </p>
-    <p> Test 2 </p>
+    <p> Commit </p>
+    <p> Commit </p>
   </div>
 </template>
