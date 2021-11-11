28f6a725f5aa88927fa8159a2fb77b8c9510226f
Lughnizaid <56219460+Lughnizaid@users.noreply.github.com>
2021-11-11 10:44:17 +0100

    feat: add new button

--- new.vue
+++ new.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <p> Commit </p>
-    <p> Commit </p>
-    <p> Commit </p>
-    <p> Commit </p>
-    <p> Commit </p>
-    <p> Commit </p>
-    <p> Commit </p>
+
   </div>
 </template>
